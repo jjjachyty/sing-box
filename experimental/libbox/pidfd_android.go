9f3aaf961b3d6a81e44cf8b5bbb365738fe6d1c8
luxun <luxun@aaa.com>
2026-07-30 08:53:33 +0800

    1

--- experimental/libbox/pidfd_android.go
+++ experimental/libbox/pidfd_android.go
@@ -1,19 +1,8 @@
 package libbox
 
-import (
-	"os"
-	_ "unsafe"
-)
-
 // https://github.com/SagerNet/sing-box/issues/3233
 // https://github.com/golang/go/issues/70508
 // https://github.com/tailscale/tailscale/issues/13452
-
-//go:linkname checkPidfdOnce os.checkPidfdOnce
-var checkPidfdOnce func() error
-
-func init() {
-	checkPidfdOnce = func() error {
-		return os.ErrInvalid
-	}
-}
+//
+// os.checkPidfdOnce was removed in Go 1.26 (pidfd is used unconditionally),
+// so the go:linkname hack is no longer needed or linkable.
